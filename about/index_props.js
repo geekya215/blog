43008aaedfa41da5d05227da4c0392681696153f
geekya215 <geekya215@users.noreply.github.com>
2021-01-06 10:20:34 +0000

    deploy: 2399888fadc52b2e363a31cd00382ecdd1f5c35e

---
+++ about/index_props.js
@@ -0,0 +1,35 @@
+import projectConfig from '/pagic.config.js';
+export default {
+    config: { "root": "/", ...projectConfig, branch: 'master' },
+    'pagePath': "about/README.md",
+    'layoutPath': "_layout.tsx",
+    'outputPath': "about/index.html",
+    'title': undefined,
+    'content': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: ''
+        } }),
+    'head': null,
+    'script': React.createElement(React.Fragment, null,
+        React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
+        React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
+        React.createElement("script", { src: "/index.js", type: "module" })),
+    'contentTitle': undefined,
+    'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: ''
+        } }),
+    'toc': null,
+    'author': "Lin Yang",
+    'contributors': [
+        "Lin Yang"
+    ],
+    'date': "2021-01-06T10:18:24.000Z",
+    'updated': null,
+    'excerpt': "",
+    'cover': undefined,
+    'blog': {
+        "isPost": false,
+        "posts": [],
+        "categories": [],
+        "tags": []
+    }
+};
---
+++ pagic.config.js
@@ -0,0 +1,43 @@
+export default {
+    srcDir: 'src',
+    theme: 'blog',
+    plugins: ['blog'],
+    title: '璃杏的博客',
+    description: '欢迎来到我的博客，这里搜集了我的技术文章和生活感悟，欢迎一起交流成长。',
+    github: 'https://github.com/geekya215/blog',
+    blog: {
+        root: '/posts/',
+        social: {
+            github: 'geekya215/blog',
+            email: 'geekya215@gmail.com',
+            twitter: 'geekya215',
+        },
+    },
+    nav: [
+        {
+            text: '首页',
+            link: '/',
+            icon: 'czs-home-l'
+        },
+        {
+            text: '分类',
+            link: '/categories/',
+            icon: 'czs-category-l'
+        },
+        {
+            text: '标签',
+            link: '/tags/',
+            icon: 'czs-tag-l'
+        },
+        {
+            text: '关于',
+            link: '/about/',
+            icon: 'czs-about-l'
+        },
+        {
+            text: '归档',
+            link: '/archives/',
+            icon: 'czs-box-l'
+        },
+    ],
+};
