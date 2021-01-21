4f5cb2887d92ed8089791e0944d3edeecc1a1123
geekya215 <geekya215@users.noreply.github.com>
2021-01-21 13:14:05 +0000

    deploy: 2bdd2d02149efea65354d331ef2976cdc6a58c1c

--- categories/index_props.js
+++ categories/index_props.js
@@ -28,8 +28,37 @@ export default {
     'cover': undefined,
     'blog': {
         "isPost": false,
-        "posts": [],
-        "categories": [],
-        "tags": []
+        "posts": [
+            {
+                "pagePath": "posts/archlinux_tutorial.md",
+                "title": "Arch Linux 完全指北",
+                "link": "posts/archlinux_tutorial.html",
+                "date": "2021-01-19T00:00:00.000Z",
+                "updated": null,
+                "author": "geekya215",
+                "contributors": [
+                    "Lin Yang"
+                ],
+                "categories": [
+                    "Linux"
+                ],
+                "tags": [
+                    "总结思考"
+                ],
+                "excerpt": "此文为博主自身在安装 Arch Linux 过程中的总结。主要参考 Arch Wiki，本文章具有一定时效性，里面的内容可能在以后的某个时间失效。 如果遇到错误，请参考最新版本的官方文档，或通过通过网络寻求帮助。 本文默认安装单系统，..."
+            }
+        ],
+        "categories": [
+            {
+                "name": "Linux",
+                "count": 1
+            }
+        ],
+        "tags": [
+            {
+                "name": "总结思考",
+                "count": 1
+            }
+        ]
     }
 };
