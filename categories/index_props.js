ad1a5425e6f4b9f9e9d44022455e73e5776551b5
geekya215 <geekya215@users.noreply.github.com>
2021-02-01 10:36:59 +0000

    deploy: e98a2c42282f31d782ffe804f1ae153517dbebb5

--- categories/index_props.js
+++ categories/index_props.js
@@ -34,7 +34,7 @@ export default {
                 "title": "Arch Linux 完全指北",
                 "link": "posts/archlinux_tutorial.html",
                 "date": "2021-01-19T00:00:00.000Z",
-                "updated": "2021-01-31T03:54:42.000Z",
+                "updated": "2021-02-01T10:34:47.000Z",
                 "author": "geekya215",
                 "contributors": [
                     "Lin Yang"
@@ -45,7 +45,7 @@ export default {
                 "tags": [
                     "总结思考"
                 ],
-                "excerpt": "此文为博主自身在安装 Arch Linux 过程中的总结。主要参考 Arch Wiki，本文章具有一定时效性，里面的内容可能在以后的某个时间失效。 如果遇到错误，请参考最新版本的官方文档，或通过通过网络寻求帮助。 本文默认安装单系统，..."
+                "excerpt": "此文为博主自身在安装 Arch Linux 过程中的总结。主要参考 Arch Wiki，本文章具有一定时效性，里面的内容可能在以后的某个时间失效。如果遇到错误，请参考最新版本的官方文档，或通过网络寻求帮助。 本文系统使用 UEFI 模式启动..."
             }
         ],
         "categories": [
